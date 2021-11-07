aa03e78c6da46e2b55c42f33b65f7434a21ed259
BennyThink <benny.think@gmail.com>
2021-11-07 10:20:29 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python3
+# coding: utf-8
+
+# deye - setup.py.py
+# 11/7/21 10:15
+#
+
+__author__ = "Benny <benny.think@gmail.com>"
+
+from setuptools import setup
+
+setup(
+    name='deye',
+    version='0.0.1',
+    py_modules=['deye','cli','config','mqtt','storage'],
+    install_requires=[
+        'Click',
+        'requests',
+        'paho-mqtt'
+    ],
+    entry_points='''
+        [console_scripts]
+        deye=cli:main
+    ''',
+)
